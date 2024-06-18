07de1ce23346fa92e1ae0e8b1c9edfddfa85c9d7
Pranaav Honnavalli <pranaav.honnavalli@briq.com>
2024-06-18 15:49:01 +0200

    chore: Update wc-tool to count lines in a file

--- ccwc.py
+++ ccwc.py
@@ -10,21 +10,19 @@ def main():
     parser.add_argument('-w',action='store_true',help='This is a flag to provide the number of words in the file or standard input')
     parser.add_argument('filename', type=str,nargs=1)
     args = parser.parse_args()
-    print(args._get_kwargs())
 
-    # Read from a file
-    f = open(args.filename[0])
     
     if args.c == True:
         print(os.stat(args.filename[0]).st_size, end=' ')
     if args.l == True:
-        print()
+        f = open(args.filename[0])
+        print(len(f.readlines()), end=' ')
     if args.w == True:
         print()
-    if args.c == False & args.l == False & args.w == False:
-        print(os.stat(args.filename[0]).st_size)
-        print()
-        print()
+    if args.c == False and args.l == False and args.w == False:
+        f = open(args.filename[0])
+        print(os.stat(args.filename[0]).st_size, end=' ')
+        print(len(f.readlines()), end=' ')
     print(args.filename[0])
 
 
