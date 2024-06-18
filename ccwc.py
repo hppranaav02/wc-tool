a2c0f345ab2545e240bf6afb0b7c16b851fe8ff8
Pranaav Honnavalli <pranaav.honnavalli@briq.com>
2024-06-18 16:18:26 +0200

    chore: Refactor ccwc.py to improve code readability and efficiency

--- ccwc.py
+++ ccwc.py
@@ -3,6 +3,7 @@ import os
 
 # driver function
 def main():
+
     # Set the arguments
     parser = argparse.ArgumentParser('wc','This is used to get a count of lines, words and bytes in a file or from standard input')
     parser.add_argument('-c',action='store_true',help='This is a flag used to get the byte count of the file or standard output')
@@ -10,22 +11,35 @@ def main():
     parser.add_argument('-w',action='store_true',help='This is a flag to provide the number of words in the file or standard input')
     parser.add_argument('filename', type=str,nargs=1)
     args = parser.parse_args()
-
+    print('    ', end='')
     
     if args.c == True:
-        print(os.stat(args.filename[0]).st_size, end=' ')
+        print(getBytes(args.filename[0]), end=' ')
+
     if args.l == True:
-        f = open(args.filename[0])
-        print(len(f.readlines()), end=' ')
+        print(getLines(args.filename[0]), end=' ')
+
     if args.w == True:
-        print()
+        print(getWords(args.filename[0]), end=' ')
+
     if args.c == False and args.l == False and args.w == False:
-        f = open(args.filename[0])
-        print(os.stat(args.filename[0]).st_size, end=' ')
-        print(len(f.readlines()), end=' ')
+        print(getLines(args.filename[0]), end='   ')
+        print(getWords(args.filename[0]), end='  ')
+        print(getBytes(args.filename[0]), end=' ')
     print(args.filename[0])
 
+def getLines(fileName):
+    f = open(fileName)
+    return len(f.readlines())
 
+def getBytes(fileName):
+    return os.stat(fileName).st_size
 
+def getWords(fileName):
+    count = 0;
+    with open(fileName) as file:
+        for line in file.readlines():
+            count += len(line.rstrip().split())
+    return count
 
 main()
